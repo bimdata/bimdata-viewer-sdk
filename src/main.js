e229208187faf941fc9caca72002349ce39b2e9f
amoki <hugo@bimdata.io>
2020-01-20 16:56:59 +0100

    configure i18n

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
+Vue.use(VueI18n)
+
+
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages: {en : {}, fr: {}}, // set locale messages
+})
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
