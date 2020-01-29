36f0986e9557734cd7337392e084b388fe4fc93e
Amoki <hugo.duroux@gmail.com>
2020-01-29 11:40:57 +0100

    allow custom url

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,13 +1,15 @@
 const authority = process.env.VUE_APP_BIMDATA_OIDC_URL || 'https://iam.bimdata.io/auth/realms/bimdata';
 
 
-const clientId = process.env.VUE_APP_CLIENT_ID
+const appUrl = process.env.VUE_APP_URL || 'http://localhost:8080';
+
+const clientId = process.env.VUE_APP_CLIENT_ID;
 
 if (!clientId) {
     throw new Error('You must define a CLIENT_ID. Have you generated your .env file as explained in the README.md?')
 }
 export const oidcSettings = {
-    redirectUri: 'http://localhost:8080/oidc-callback',
+    redirectUri: appUrl + '/oidc-callback',
     authority,
     clientId,
     responseType: 'code',
