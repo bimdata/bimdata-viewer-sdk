c65e0594f3b0d522253b16c45f95fec21eef1797
Amoki <hugo.duroux@gmail.com>
2020-03-05 10:40:49 +0100

    typo

--- tools/init-plugin.js
+++ tools/init-plugin.js
@@ -117,7 +117,7 @@ const run = async () => {
 
   console.log(chalk.green('You can now load your plugin to the viewer:'));
   console.log(chalk.green('Add the following lines to src/views/viewer.vue'));
-  console.log(`import test from "@/plugins/${answers.name}/src/${answers.name}.plugin.js";`);
+  console.log(`import ${answers.name} from "@/plugins/${answers.name}/src/${answers.name}.plugin.js";`);
   console.log(`this.$refs.bimdataViewerInstance.registerPlugins([${answers.name}]);`);
 };
 
