7221673b9a3a942010f55a5867735b2797de21fb
Gaelle <gaelle@bimdata.io>
2020-07-17 11:07:21 +0200

    add nodeResolve & commonjs for rollup config
    for resolve BIMDataDesignSystem & chartist lib import in Iot & Graph when build plugin

--- src/plugins/iot/rollup.config.js
+++ src/plugins/iot/rollup.config.js
@@ -2,12 +2,16 @@ import VuePlugin from 'rollup-plugin-vue';
 import scssPlugin from 'rollup-plugin-scss';
 import url from '@rollup/plugin-url';
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 const isProduction = (process.env.BUILD === 'production');
 
 const plugins = [
   VuePlugin(),
   scssPlugin(),
+  nodeResolve(),
+  commonjs(),
   url({
     limit: 100 * 1024 *1024,
     include: ['assets/*']
