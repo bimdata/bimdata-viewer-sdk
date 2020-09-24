bd6d7e5d72e928d6105784ecbf13af310ed67b03
Gaelle <gaelle@bimdata.io>
2020-09-24 18:56:00 +0200

    create kroqi modal plugin

---
+++ src/plugins/modalKroqi/src/ModalKroqi.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="kroqi-modal">
+    <div class="kroqi-modal__header flex items-center justify-center">
+      <img src="../assets/img-square-blur.svg" alt="" class="square-blur" />
+      <img src="../assets/logo-bimdata.svg" alt="" />
+      <img src="../assets/img-square-3d.svg" alt="" class="square-3d" />
+    </div>
+    <div class="kroqi-modal__content">
+      <div class="flex">
+        <div
+          class="kroqi-modal__content__card flex flex-col items-center justify-center m-x-24"
+        >
+          <img src="../assets/img-bcf-manager.svg" alt="" />
+          <h5>{{ $t("modalKorqiPlugin.titleBcf") }}</h5>
+        </div>
+        <div
+          class="kroqi-modal__content__card flex flex-col items-center justify-center m-x-24"
+        >
+          <img src="../assets/img-edit-ifc.svg" alt="" />
+          <h5>{{ $t("modalKorqiPlugin.titleEditIfc") }}</h5>
+        </div>
+        <div
+          class="kroqi-modal__content__card flex flex-col items-center justify-center m-x-24"
+        >
+          <img src="../assets/img-viewer.svg" alt="" />
+          <h5>{{ $t("modalKorqiPlugin.titleViewer") }}</h5>
+        </div>
+      </div>
+      <div class="flex items-center justify-center">
+        <a
+          :href="bimdata_url()"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="bimdata-btn bimdata-btn__fill bimdata-btn__fill--secondary bimdata-btn__radius"
+          >{{ $t("modalKorqiPlugin.btnSpace") }}</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalKroqi",
+  methods: {
+    bimdata_url() {
+      const apiUrl = this.$store.state.viewer.viewerComponent.cfg.apiUrl;
+      if (apiUrl.includes("staging")) {
+        return "https://platform-staging.bimdata.io/";
+      } else if (apiUrl.includes("next")) {
+        return "https://platform-next.bimdata.io/";
+      }
+      return "https://platform.bimdata.io/";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../node_modules/@bimdata/design-system/dist/scss/_BIMDataVariables.scss";
+.kroqi-modal {
+  &__header {
+    padding: 40px;
+    position: relative;
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      background-color: $color-primary;
+      clip-path: polygon(0 0, 100% 0, 100% 65%, 0 100%);
+    }
+    img {
+      position: relative;
+    }
+    .square-blur {
+      position: absolute;
+      left: 15%;
+      bottom: 10%;
+    }
+    .square-3d {
+      position: absolute;
+      right: 15%;
+      top: 20%;
+    }
+  }
+  &__content {
+    padding: 40px;
+    &__card {
+      max-width: 210px;
+      h5 {
+        min-height: 46px;
+        font-size: 18px;
+        position: relative;
+        line-height: 23px;
+        text-align: center;
+        &::before {
+          width: 20px;
+          height: 3px;
+          content: "";
+          position: absolute;
+          background-color: $color-secondary;
+          top: -20px;
+          left: calc(50% - 10px);
+        }
+      }
+    }
+    .bimdata-btn {
+      padding: 0 12px;
+      height: 32px;
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ src/plugins/modalKroqi/src/modalKroqi.plugin.js
@@ -0,0 +1,36 @@
+import btnModalKroqiComponent from "./BtnModalKroqi.vue";
+import icon from "../assets/icon.svg";
+
+export default {
+  name: "btnModalKroqiComponent",
+  component: btnModalKroqiComponent,
+  display: {
+    iconPosition: "right",
+    content: "free",
+  },
+  tooltip: "modalKorqiPlugin.tooltip",
+  keepActive: false,
+  icon: {
+    imgUri: icon,
+  },
+  i18n: {
+    en: {
+      modalKorqiPlugin: {
+        tooltip: "Kroqi",
+        titleBcf: "BCF manager (import / export)",
+        titleEditIfc: "Edit properties and export ifc",
+        titleViewer: "A 3D / 2D BIM viewer",
+        btnSpace: "Access your space",
+      },
+    },
+    fr: {
+      modalKorqiPlugin: {
+        tooltip: "Kroqi",
+        titleBcf: "BCF manager (import / export)",
+        titleEditIfc: "Editer des propriétés et exporter des ifc",
+        titleViewer: "Une visionneuse BIM 3D/2D ",
+        btnSpace: "Accéder à votre espace",
+      },
+    },
+  },
+};
