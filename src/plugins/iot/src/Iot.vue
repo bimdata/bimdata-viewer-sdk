debd4dadf14eba02064b2f16c116793cafb695ca
Gaelle <gaelle@bimdata.io>
2020-11-17 13:48:11 +0100

    IOT: change path to scss style && bump to 1.0.4

--- src/plugins/iot/src/Iot.vue
+++ src/plugins/iot/src/Iot.vue
@@ -246,8 +246,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../node_modules/chartist/dist/chartist.min.css";
-@import "../node_modules/@bimdata/design-system/dist/scss/BIMData.scss";
+@import "./node_modules/chartist/dist/chartist.min.css";
+@import "./node_modules/@bimdata/design-system/dist/scss/BIMData.scss";
 
 .bimdata-iot {
   .select {
