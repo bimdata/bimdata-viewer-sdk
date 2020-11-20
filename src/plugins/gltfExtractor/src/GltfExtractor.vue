036d465db6469d1821cc2da0685d8235cbefb76a
Gaelle <gaelle@bimdata.io>
2020-11-20 11:18:43 +0100

    change @bimdata/ds version into GltfExtractor plugin

--- src/plugins/gltfExtractor/src/GltfExtractor.vue
+++ src/plugins/gltfExtractor/src/GltfExtractor.vue
@@ -1,27 +1,26 @@
 <template>
-  <!-- https://vuejs.org/v2/guide/syntax.html -->
   <div>
-    <button @click="downloadGltf">Download GLTF</button>
+    <BIMDataButton color="primary" fill radius @click="downloadGltf"
+      >Download GLTF</BIMDataButton
+    >
   </div>
 </template>
 
 <script>
+import { BIMDataButton } from "../node_modules/@bimdata/design-system/components.js";
 export default {
-  // https://vuejs.org/v2/guide/components.html
   name: "gltfExtractor",
+  components: {
+    BIMDataButton,
+  },
   data() {
-    return {
-    }
+    return {};
   },
   methods: {
     downloadGltf() {
       const ifc = this.$viewer.state.ifcs[0];
       window.location.href = ifc.gltf_file;
-    }
-  }
+    },
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-/* https://vue-loader.vuejs.org/guide/scoped-css.html#mixing-local-and-global-styles */
-</style>
