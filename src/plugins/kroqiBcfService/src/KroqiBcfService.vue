9d06b80d0519e7229af41f83c4d52f722c384755
LrxGaelle <gaelle@bimdata.io>
2021-04-13 14:30:03 +0200

    add style for kroqi bcf plugin (#59)

--- src/plugins/kroqiBcfService/src/KroqiBcfService.vue
+++ src/plugins/kroqiBcfService/src/KroqiBcfService.vue
@@ -1,28 +1,32 @@
 <template>
   <!-- https://vuejs.org/v2/guide/syntax.html -->
-  <div class="bcf-premium flex flex-col justify-center items-center">
-    <img class="m-b-24" src="../assets/logo-bimdata.svg" alt="" />
-    <h6 class="bimdata-h6 color-primary primary-font">Viewer BCF BIM Server</h6>
-    <p class="bimdata-text color-tertiary-darkest primary-font m-t-12">
-      Premier projet gratuit, 45.00 € ht par projet par mois pour les projets
-      suivants.
-    </p>
-    <BIMDataButton
-      v-if="userIsAdmin"
-      class="large-btn m-t-24"
-      color="secondary"
-      fill
-      radius
-      @click="open"
-    >
-      <BIMDataIcon name="chevron" size="xxxs" />
-      <span>{{ $t("bcfKroqiPremiumService.activate") }}</span>
-    </BIMDataButton>
-    <p class="bimdata-text color-tertiary-darkest primary-font m-t-12" v-else>
-      Veuillez contacter la personne qui administre votre organisation Kroqi
-      pour activer cette fonctionnalité.
-    </p>
-    <p class="bimdata-text color-tertiary-dark primary-font m-t-12">
+  <div class="bcf-premium flex flex-col justify-around items-center">
+    <div class="bcf-premium__content flex flex-col justify-center items-center">
+      <img class="m-b-24" src="../assets/logo-bimdata.svg" alt="" />
+      <h6 class="bimdata-h6 color-primary primary-font">
+        Viewer BCF BIM Server
+      </h6>
+      <p class="color-tertiary-darkest primary-font m-t-18">
+        Premier projet gratuit, <strong>45.00€ ht</strong> par projet par mois
+        pour les projets suivants.
+      </p>
+      <BIMDataButton
+        v-if="userIsAdmin"
+        class="large-btn m-t-18"
+        color="secondary"
+        fill
+        radius
+        @click="open"
+      >
+        <BIMDataIcon name="chevron" size="xxxs" />
+        <span>{{ $t("bcfKroqiPremiumService.activate") }}</span>
+      </BIMDataButton>
+      <p class="color-tertiary-darkest primary-font m-y-18" v-else>
+        Veuillez contacter la personne qui administre votre organisation Kroqi
+        pour activer cette fonctionnalité.
+      </p>
+    </div>
+    <p class="color-tertiary-dark primary-font m-y-24">
       Vous devrez recharger cette page après activation pour accéder au service
       BCF.
     </p>
@@ -61,7 +65,16 @@ export default {
 @import "../node_modules/@bimdata/design-system/dist/scss/utilities/text.scss";
 .bcf-premium {
   height: 100%;
-  padding: 0 90px;
+  padding: 0 80px;
   text-align: center;
+  &__content {
+    height: 80%;
+  }
+  p {
+    line-height: 1.4;
+  }
+  p.color-tertiary-dark {
+    font-size: 12px;
+  }
 }
 </style>
