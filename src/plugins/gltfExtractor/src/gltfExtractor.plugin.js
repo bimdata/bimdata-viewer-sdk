90ccbcba9c1ce0b9ae3f3a8ae6d799384246632f
LrxGaelle <gaelle@bimdata.io>
2021-04-29 18:29:56 +0200

    Fix/transparent btn (#60)
    * change tooltip & @bimdata/design-system version for svgExtractor plugin
    
    * change tooltip & @bimdata/design-system version for gltfExtractor plugin

--- src/plugins/gltfExtractor/src/gltfExtractor.plugin.js
+++ src/plugins/gltfExtractor/src/gltfExtractor.plugin.js
@@ -1,25 +1,27 @@
-import GltfExtractorComponent from './GltfExtractor.vue';
-import icon from '../assets/export-gltf.svg';
+import GltfExtractorComponent from "./GltfExtractor.vue";
+import icon from "../assets/export-gltf.svg";
 
 export default {
-  name: 'GltfExtractorPlugin',
+  name: "GltfExtractorPlugin",
   component: GltfExtractorComponent,
   addToWindows: ["3d"],
   button: {
-    position: 'right',
-    content: 'simple', // How the viewer show the plugin content. Value can be "windowed", "simple" of "free"
+    position: "right",
+    content: "simple", // How the viewer show the plugin content. Value can be "windowed", "simple" of "free"
     keepOpen: true,
-    tooltip: 'GltfExtractorPlugin.tooltip', // Can be an i18n path or a string shown in all languages
+    tooltip: "GltfExtractorPlugin.tooltip", // Can be an i18n path or a string shown in all languages
     icon: {
       imgUri: icon,
     },
   },
   i18n: {
     en: {
-      tooltip: 'Download GLTF of the model'
+      tooltip: "Download GLTF of the model",
+      action: "Download GLTF",
     },
     fr: {
-      tooltip: 'Téléchargez le GLTF du modèle'
-    }
-  }
+      tooltip: "Téléchargez le GLTF du modèle",
+      action: "Télécharger le GLTF",
+    },
+  },
 };
