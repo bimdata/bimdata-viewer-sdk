e9f94ac667e9d4f03c65e97ea3cb639c3ae26d96
Hugo Duroux <hugo@bimdata.io>
2022-09-12 18:36:59 +0200

    updade viewer (#100)
    * updade viewer
    
    * remove dead code

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,10 @@
-import * as bimdata from "@bimdata/bimdata-api-client";
+import { makeBIMDataApiClient } from "@bimdata/typescript-fetch-api-client";
 
 export default function getClient({ accessToken, apiUrl }) {
-  const defaultClient = bimdata.ApiClient.instance;
-  defaultClient.basePath = apiUrl;
-  const Bearer = defaultClient.authentications["Bearer"];
-
-  Bearer.type = "oauth2";
-  Bearer.accessToken = accessToken;
-
-  return bimdata;
+  const apiClient = makeBIMDataApiClient({
+    apiUrl: apiUrl,
+    credentials: "include"
+  });
+  apiClient.accessToken = accessToken
+  return apiClient;
 }
