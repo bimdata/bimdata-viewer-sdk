7e7ec705481188b3b4eb767162459221ed5004c1
Amoki <hugo@bimdata.io>
2022-09-12 18:36:47 +0200

    remove dead code

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,5 @@
 import { makeBIMDataApiClient } from "@bimdata/typescript-fetch-api-client";
 
-makeBIMDataApiClient({
-  apiUrl: process.env.VUE_APP_API_BASE_URL,
-  credentials: "include"
-});
-
 export default function getClient({ accessToken, apiUrl }) {
   const apiClient = makeBIMDataApiClient({
     apiUrl: apiUrl,
