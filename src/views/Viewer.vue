41cc6ea9687b067473d57ed26f1e3f196fedd491
Gaelle <gaelle@bimdata.io>
2023-02-06 17:02:02 +0100

    add plugins to Viewer view

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -8,20 +8,19 @@
 import { mapGetters } from "vuex";
 import makeBIMDataViewer from "@bimdata/viewer";
 
-import backgroundColor from "@/plugins/backgroundColor/src/backgroundColor.plugin.js";
-import BimObjectPlugin from "@/plugins/bimobject/src/bimobject.plugin.js";
-import bsdd from "@/plugins/bsdd/src/bsdd.plugin.js";
-import ChristmasSleighPlugin from "@/plugins/christmasSleigh/src/christmasSleigh.plugin.js";
-import excelExportPlugin from "@/plugins/excelExport/src/excelExport.plugin.js";
-import GltfExtractorPlugin from "@/plugins/gltfExtractor/src/gltfExtractor.plugin.js";
-import HolusionPlugin from "@/plugins/holusion/src/holusion.plugin.js";
-import kroqiBcfService from "@/plugins/kroqiBcfService/src/kroqiBcfService.plugin.js";
-import platformDemo from "@/plugins/platformDemo/src/platformDemo.plugin.js";
-import SnowflakesPlugin from "@/plugins/snowflakes/src/snowflakes.plugin.js";
-import SplitPlugin from "@/plugins/split/src/split.plugin.js";
-import SvgExtractorPlugin from "@/plugins/svgExtractor/src/svgExtractor.plugin.js";
-import Giro3dPlugin from "@/plugins/giro3d/src/giro3d.plugin.js";
-import IotPlugin from "@/plugins/iot/src/iot.plugin.js";
+import backgroundColor from "@/plugins/backgroundColor/dist/backgroundColor.plugin.js";
+import BimObjectPlugin from "@/plugins/bimobject/dist/bimobject.plugin.js";
+import bsdd from "@/plugins/bsdd/dist/bsdd.plugin.js";
+import ChristmasSleighPlugin from "@/plugins/christmasSleigh/dist/christmasSleigh.plugin.js";
+import excelExportPlugin from "@/plugins/excelExport/dist/excelExport.plugin.js";
+import Giro3dPlugin from "@/plugins/giro3d/dist/giro3d.plugin.js";
+import GltfExtractorPlugin from "@/plugins/gltfExtractor/dist/gltfExtractor.plugin.js";
+import HolusionPlugin from "@/plugins/holusion/dist/holusion.plugin.js";
+import kroqiBcfService from "@/plugins/kroqiBcfService/dist/kroqiBcfService.plugin.js";
+import platformDemo from "@/plugins/platformDemo/dist/platformDemo.plugin.js";
+import SnowflakesPlugin from "@/plugins/snowflakes/dist/snowflakes.plugin.js";
+import SplitPlugin from "@/plugins/split/dist/split.plugin.js";
+import SvgExtractorPlugin from "@/plugins/svgExtractor/dist/svgExtractor.plugin.js";
 
 export default {
   data() {
@@ -53,11 +52,12 @@ export default {
       },
     });
 
-    // bimdataViewer.registerPlugin(backgroundColor);
+    bimdataViewer.registerPlugin(backgroundColor);
     bimdataViewer.registerPlugin(BimObjectPlugin);
     bimdataViewer.registerPlugin(bsdd);
     bimdataViewer.registerPlugin(ChristmasSleighPlugin);
     bimdataViewer.registerPlugin(excelExportPlugin);
+    bimdataViewer.registerPlugin(Giro3dPlugin);
     bimdataViewer.registerPlugin(GltfExtractorPlugin);
     bimdataViewer.registerPlugin(HolusionPlugin);
     bimdataViewer.registerPlugin(kroqiBcfService);
