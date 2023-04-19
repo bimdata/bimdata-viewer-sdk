76878f91ef9b55689e6177de8c3f706f27b8c764
Kurtil <gaetan.lagier@hotmail.fr>
2023-04-19 17:35:48 +0200

    fix bimworld plugin

--- src/plugins/bimworld/src/Bimworld.js
+++ src/plugins/bimworld/src/Bimworld.js
@@ -23,42 +23,23 @@ export default {
     });
   },
   methods: {
-    getUIElements() {
-      const uiElements = [];
-
-      const headerPlugin = this.$viewer.localContext.plugins.get("header");
-      if (headerPlugin) {
-        uiElements.push(headerPlugin.$el);
-      }
-      Array.from(this.$viewer.localContext.plugins.values())
-        .filter(plugin => plugin.$plugin.button)
-        .forEach(plugin => {
-          const parent = plugin.$parent.$el.classList.contains("panel")
-            ? plugin.$parent.$parent.$parent // button plugin in panel
-            : plugin.$parent;
-          uiElements.push(parent.$el);
-        });
-
-      return uiElements;
-    },
     hideUI() {
-      this.getUIElements().forEach(uiElement => {
-        uiElement.style.visibility = "hidden";
-      });
+      const exceptions = ["viewer3d"];
+      [...this.$viewer.localContext.plugins.entries()]
+        .filter(([name]) => !exceptions.includes(name))
+        .forEach(([, plugin]) => plugin.$hide());
     },
     showUI() {
-      this.getUIElements().forEach(uiElement => {
-        uiElement.style.visibility = "";
-      });
+      this.$viewer.localContext.plugins.forEach(plugin => plugin.$show())
     },
     startPivot() {
-      const xeokit = this.$viewer.localContext.getPlugin('viewer3d').xeokit;
+      const xeokit = this.$viewer.localContext.plugins.get('viewer3d').xeokit;
       this.pivotEvent = xeokit.scene.on('tick', ({deltaTime}) => {
         xeokit.scene.camera.orbitYaw(0.01 * deltaTime);
       });
     },
     stopPivot() {
-      const xeokit = this.$viewer.localContext.getPlugin('viewer3d').xeokit;
+      const xeokit = this.$viewer.localContext.plugins.get('viewer3d').xeokit;
       if(this.pivotEvent) {
         xeokit.scene.off(this.pivotEvent);
       }
