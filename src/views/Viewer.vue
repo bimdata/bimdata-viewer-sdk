a83bac09341d47e37fd932cb934ecfda86b31612
Kurtil <gaetan.lagier@hotmail.fr>
2023-04-20 10:35:22 +0200

    update christmasSleight plugin to 3.0.0

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -10,7 +10,7 @@ import makeBIMDataViewer from "@bimdata/viewer";
 
 import BimObjectPlugin from "@/plugins/bimobject/dist/bimobject.plugin.esm.min.js";
 import bsdd from "@/plugins/bsdd/dist/bsdd.plugin.esm.min.js";
-import ChristmasSleighPlugin from "@/plugins/christmasSleigh/dist/christmasSleigh.plugin.js";
+import ChristmasSleighPlugin from "@/plugins/christmasSleigh/dist/christmasSleigh.plugin.esm.min.js";
 import excelExportPlugin from "@/plugins/excelExport/dist/excelExport.plugin.js";
 import Giro3dPlugin from "@/plugins/giro3d/dist/giro3d.plugin.js";
 import GltfExtractorPlugin from "@/plugins/gltfExtractor/dist/gltfExtractor.plugin.js";
